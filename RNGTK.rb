bede03b7ee0dc19c000cf86ba9dece10a2e4d42d
hubertkarbowy <hk@hubertkarbowy.pl>
2017-08-18 22:40:54 +0200

    Various fixes and Katz Backoff

--- RNGTK.rb
+++ RNGTK.rb
@@ -6,19 +6,17 @@
 ## Version: 0.93a
 ## Requires Ruby 2.4.0 - might work with 2.3 and 2.2, won't work with lower versions
 ##
+## For a demonstration run scratchpad.rb
+## Please don't take Good-Turing discounted counts and discounted probabilities on faith. It is quite likely that is version still contains bugs.
+##
 ## CHANGELOG:
 ## 0.91 - fixed MLE probabilites
 ## 0.92 - fixed probabilities with Good-Turing discount
 ## 0.93a - started implementing Katz backoff (incomplete)
-##
+## 0.94 - Katz backoff complete + fixes in calculating probabilities
 
-class Ngrams
-  @k
-  @ngram_counts
-  @good_turing_bins
-
-  # inner class to color output in verbose mode
-  class String
+# class to color output in verbose mode
+class String
     def black;          "\e[30m#{self}\e[0m" end
     def red;            "\e[31m#{self}\e[0m" end
     def green;          "\e[32m#{self}\e[0m" end
@@ -29,6 +27,12 @@ class Ngrams
     def gray;           "\e[37m#{self}\e[0m" end
   end
 
+class Ngrams
+  @k
+  @ngram_counts
+  @good_turing_bins
+  @verbose=false
+
   # corpus - filename, max_ngram model - self-explanatory, k - for GT smoothing
   # strip_punctuation - removes non-alphabetical chars
   # ignorecase - downcases everything
@@ -36,7 +40,7 @@ class Ngrams
     start_time=Time.now
 
     corpus_file = File.read(corpus).encode('UTF-8', 'UTF-8', invalid: :replace, undef: :replace, replace: '@')
-    corpus_file.gsub!("\n", " ").delete!("\r").gsub!(/ +/, " ") unless normalize == :by_line
+    corpus_file = corpus_file.gsub("\n", " ").delete("\r").gsub(/ +/, " ") unless normalize == :by_line
 
     case normalize
       when :force
@@ -59,7 +63,7 @@ class Ngrams
       @ngram_counts[i] = Hash.new
       normalized_corpus.each_line do |sentence|
         temp_counts = sentence.split(" ").each_cons(i).reduce(Hash.new(0)) {|acc, word| acc[word].nil? ? acc[word]=1 : acc[word] += 1; acc }.map{|k,v| [k.join(" "), v]}.to_h
-        temp_counts.each {|token, sentencecounts| @ngram_counts[i][token].nil? ? @ngram_counts[i][token]=1 : @ngram_counts[i][token]+=sentencecounts}
+        temp_counts.each {|token, sentencecounts| @ngram_counts[i][token].nil? ? @ngram_counts[i][token]=sentencecounts : @ngram_counts[i][token]+=sentencecounts}
       end
       puts "For #{i}-grams number of types (unique tokens) = #{@ngram_counts[i].size}" if verbose
     end
@@ -84,6 +88,7 @@ class Ngrams
 
     @good_turing_bins=gt_bins
     @k=k
+    @verbose=verbose
     stop_time=Time.now
     puts "RNGTK ready. Model loaded model in #{stop_time-start_time} s." if verbose
   end
@@ -93,11 +98,12 @@ class Ngrams
     local_ngram_model = ngram_model==0 ? next_ngram.split(separator).count : ngram_model
     next_ngram_rawcount = ngram_counts[local_ngram_model][next_ngram].to_i
     if next_ngram_rawcount == 0
-      raise "Revised counts for zero raw counts don't make sense"
+      raise "Revised counts for zero raw counts don't make sense #{next_ngram}"
     elsif next_ngram_rawcount <= @k
       ordinary_gt = (next_ngram_rawcount+1)*(good_turing_bins[local_ngram_model][next_ngram_rawcount+1].to_f/good_turing_bins[local_ngram_model][next_ngram_rawcount])
       mle_provision = (next_ngram_rawcount*(@k+1)*good_turing_bins[local_ngram_model][@k+1].to_f)/good_turing_bins[local_ngram_model][1]
-      normalization = 1-(((@k+1)*good_turing_bins[local_ngram_model][@k+1].to_f)/good_turing_bins[local_ngram_model][1])
+      normalization = 1 - ((@k+1)*good_turing_bins[local_ngram_model][@k+1].to_f)/good_turing_bins[local_ngram_model][1]
+      warn "Good-Turing discounting with a parameter is numerically unstable. Negative revised counts were detected for #{next_ngram}." if (@verbose and ((ordinary_gt-mle_provision)/normalization)<0)
       return (ordinary_gt-mle_provision)/normalization # This is equation 4.31 from JM
     else
       return (next_ngram_rawcount)
@@ -109,7 +115,7 @@ class Ngrams
   ## When mode is set to :single (default) - the phrase is assumed to be an n-gram already (n is inferred from separator)
   ## When mode is set to :sentence - the phrase is split into n-grams and their MLEs are multiplied.
   ## Todo: Consider removing mode parameter - it might interfere with external calls
-  def calculate_mle_probabilitity phrase: nil, ngram_model: 0, separator: " ", mode: :single
+  def calculate_mle_probability next_ngram: nil, ngram_model: 0, separator: " ", mode: :single
     raise ('MLE: ngram_model must be set explicitly in sentence mode') if (ngram_model==0 and mode == :sentence)
 
     local_ngram_model=ngram_model==0 ? phrase.split(separator).count : ngram_model
@@ -140,15 +146,61 @@ class Ngrams
     local_ngram_model = ngram_model==0 ? prior.split(separator).count + 1 : ngram_model # plus one because of the posterior
     raise "N-gram #{prior}: conditional probabilities for unigrams don't make sense" if local_ngram_model==1
     prior_counts = ngram_counts[local_ngram_model-1][prior].to_i
-    raise "#{prefix}: Unable to calculate conditional probability for prior counts = 0. Perhaps you meant to use Katz-backoff?" if prior_counts==0
-
-    if discounted
+    raise "#{prior}: Unable to calculate conditional probability for prior counts = 0. Perhaps you meant to use Katz-backoff?" if prior_counts==0
+    ngram_raw_counts = ngram_counts[local_ngram_model][prior+separator+posterior].to_f
+    
+    if discounted and ngram_raw_counts<=@k
       ngram_discounted_counts = get_revised_counts(next_ngram: prior+separator+posterior, ngram_model: local_ngram_model)
       return ngram_discounted_counts/prior_counts
     else
-      ngram_raw_counts = ngram_counts[local_ngram_model][prior+separator+posterior].to_f
       return ngram_raw_counts/prior_counts
     end
+  end
+  
+  def calculate_alpha next_ngram: nil, ngram_model:0, ngram_counts: @ngram_counts, good_turing_bins: @good_turing_bins, separator: " "
+      local_ngram_model = ngram_model==0 ? next_ngram.split(separator).count : ngram_model
+      backoff_ngram = next_ngram.split(separator)[1..-1].join(separator) # e.g. "Your house looks ugly" ---> "house looks ugly"
+      prefix_regex = /^#{next_ngram.split(separator)[0..-2].join(separator)}\b/
+      prefix = next_ngram.split(separator)[0..-2].join(separator)
+      puts "  Prefix: #{prefix}. Calculating α(#{prefix})" if @verbose
+
+      return 1 if ngram_counts[local_ngram_model-1][prefix].to_i==0
+      
+      raise "Nothing to sum for N=1" if local_ngram_model==1
+      puts "      Calculating β(#{prefix.to_s})" if @verbose
+      beta_sum_of_probabilities = 0
+      all_cond_ngrams = ngram_counts[local_ngram_model].keys.select {|k| k.match(prefix_regex)}.map{|k| k.split(separator).last} # e.g. (in her) FORTIES, (in her) HOUSE, (in her) JEANS - returns only the words in parentheses      
+      all_cond_ngrams.each do |last_word|
+          cond_ngram = prefix + separator + last_word
+          printf "       > Considering #{p_cond(phrase: cond_ngram).green}" if @verbose
+          last_word_cond_probability = calculate_conditional_probability(posterior: last_word, prior: prefix, discounted: true)
+          beta_sum_of_probabilities += last_word_cond_probability
+          printf " = #{last_word_cond_probability}\n" if @verbose
+      end
+
+      beta = 1 - beta_sum_of_probabilities
+      puts "        * Sum of revised probabilities #{p_cond(phrase: prefix+' ●').green}: " if @verbose
+      puts "        * β(#{prefix.green}) = #{beta}"
+      
+      return beta if local_ngram_model==2 # for bigrams
+      
+      puts "       Now moving on to lower-order n-grams #{backoff_ngram.split(separator)[0..-2].join(" ").brown+' ●'.brown}" if @verbose
+      all_similar = ngram_counts[local_ngram_model-1].select {|k,v| k.match(/^#{backoff_ngram.split(separator)[0..-2].join(separator)}\b/)} # .map{|k| k.split.last} # e.g. IN HER FORTIES, IN HER HOUSE, IN HER JEANS
+      all_similar.delete_if {|k,v| !ngram_counts[local_ngram_model][prefix+" "+k.split(separator).last].nil? }
+      sum_of_backoff_counts = 0
+      similar_ngram_gt_probabilities = 0
+      # revised_count_of_backoff_ngram = get_revised_counts(next_ngram: backoff_ngram, ngram_model: local_ngram_model-1, separator: separator)
+      all_similar.each do |alt_phrase, rawcount| 
+        similar_ngram = "#{alt_phrase}" # IN HER FORTIES, IN HER JEANS, IN HER HOUSE...
+        similar_ngram_gt_probabilities += calculate_conditional_probability(posterior: alt_phrase.split(separator).last, prior: alt_phrase.split(separator)[0..-2].join(separator), discounted: true) 
+        #similar_ngram_rawcount += rawcount
+        similar_ngram_revised_count = get_revised_counts(next_ngram: similar_ngram)
+        # sum_of_backoff_counts += similar_ngram_revised_count
+        puts "       Considering #{p_cond(phrase: alt_phrase).brown}, P* = #{calculate_conditional_probability(posterior: alt_phrase.split(separator).last, prior: alt_phrase.split(separator)[0..-2].join(separator), discounted: true)}" if @verbose
+      end
+      puts "         * Sum of revised probabilities #{p_cond(phrase: backoff_ngram.split(separator)[0..-2].join(" ")+' ●').brown} is #{similar_ngram_gt_probabilities}" if @verbose
+      puts "         * α = #{beta/1-similar_ngram_gt_probabilities}" if @verbose
+      return beta / (1-similar_ngram_gt_probabilities)
   end
 
   def calculate_katz_probability next_ngram: nil, ngram_model:0, ngram_counts: @ngram_counts, good_turing_bins: @good_turing_bins, separator: " "
@@ -165,7 +217,8 @@ class Ngrams
       backoff_ngram=next_ngram.split(separator)[1..-1].join(separator)
       prior = next_ngram.split(separator)[1..-2].join(separator)
       posterior = next_ngram.split(separator)[-1]
-      alpha=0.5 # Todo: Algorithm for calculating alpha
+      alpha = calculate_alpha(next_ngram: next_ngram, ngram_model: local_ngram_model)
+      puts "          BACKING OFF TO: #{backoff_ngram}\n\n" if @verbose 
       return alpha*calculate_katz_probability(next_ngram: backoff_ngram, ngram_model: local_ngram_model-1, separator: separator)
     end
 
@@ -176,8 +229,14 @@ class Ngrams
     ngram_model_inferred = ngram_model==0 ? phrase.split(separator).count : ngram_model
     return @ngram_counts[ngram_model_inferred][phrase]
   end
+  
+  def p_cond phrase: nil
+    arr=phrase.split
+    return ("P(#{arr.last} | #{arr[0..-2].join(" ")})")
+  end
 
   def get_gt_bins; return @good_turing_bins; end
   def get_ngram_counts; return @ngram_counts; end
-
+  def be_quiet; @verbose=false; end
+  def be_verbose; @verbose=true; end
 end
--- scratchpad.rb
+++ scratchpad.rb
@@ -9,32 +9,41 @@
 ##
 ## Requires RNGTK v. 0.93a and Ruby 2.4.0 - might work with 2.3 and 2.2, won't work with lower versions
 
+# This program demonstrates RNGTK
+# Please don't take Good-Turing discounted counts and discounted probabilities on faith. It is quite likely that is version still contains bugs. 
 
 require_relative './RNGTK.rb'
 require 'pp'
 
-simplemodel = Ngrams.new(corpus: 'simplecorpus.txt', max_ngram_model: 4, k: 4)
-puts simplemodel.get_revised_counts(next_ngram: 'one two three four', ngram_model: 4)
-
+simplemodel = Ngrams.new(corpus: 'churchill.txt', max_ngram_model: 3, k: 2, normalize: :by_token)
+#simplemodel = Ngrams.new(corpus: '/wymiana/Projekty/NLP/persuasion.txt', normalize: :by_token, max_ngram_model: 4, k: 4)
 ngramcounts = simplemodel.get_ngram_counts
-for n in 1..4 do
+
+begin
+for n in 1..2 do
     puts "========== #{n}-grams ==========="
-    puts ngramcounts[n]
-    ngramcounts[n].each {|k,_| puts "c*(#{k}) = #{simplemodel.get_revised_counts(next_ngram: k)}"}
-    ngramcounts[n].each {|k,_| puts "P*(#{k}) = #{simplemodel.calculate_gt_probability(next_ngram: k)}"}
+#    puts ngramcounts[n]
+#    ngramcounts[n].each {|k,_| puts "c*(#{k}) = #{simplemodel.get_revised_counts(next_ngram: k)}"}
+#    ngramcounts[n].each {|k,_| puts "P*(#{k}) = #{simplemodel.calculate_gt_probability(next_ngram: k)}"}
     totalprob=ngramcounts[n].reduce(0){|acc, (k,v)| acc += simplemodel.calculate_gt_probability(next_ngram: k)}
     prob_unk = simplemodel.calculate_gt_probability(next_ngram: "#{'unknown '*n}")
     puts " Probability (seen):  #{totalprob}\n+Probability (unseen): #{prob_unk}\n=Total probability = #{totalprob+prob_unk}"
 end
 
-puts "============ Conditional probabilities (raw) ================"
-prior = 'your house looks'
-posterior = ['fine', 'nice', 'lovely', 'good']
-posterior.each {|x| puts "P(#{x} | #{prior}) = #{simplemodel.calculate_conditional_probability(posterior: x, prior: prior, ngram_model: 4)}"}
-puts "Sum of conditional probabilities = #{posterior.reduce(0) {|acc, x| acc+=simplemodel.calculate_conditional_probability(posterior: x, prior: prior, ngram_model: 4)}}"
+puts "============ MLE conditional probabilities with raw counts ================"
+prior = 'german'
+posterior = ['forces', 'failure', 'exactions', 'aggression', 'power', 'broadcast', 'troops', 'hands', 'and', 'war', 'air']
+posterior.each {|x| puts "P(#{x} | #{prior}) = #{simplemodel.calculate_conditional_probability(posterior: x, prior: prior, ngram_model: 2)}, raw count = #{ngramcounts[2][prior+' '+x]}"}
+puts "Sum of conditional probabilities = #{posterior.reduce(0) {|acc, x| acc+=simplemodel.calculate_conditional_probability(posterior: x, prior: prior, ngram_model: 2)}}"
 
-puts "============ Conditional probabilities (discounted) ================"
-prior = 'your house looks'
-posterior = ['fine', 'nice', 'lovely', 'good']
-posterior.each {|x| puts "P(#{x} | #{prior}) = #{simplemodel.calculate_conditional_probability(posterior: x, prior: prior, ngram_model: 4, discounted: true)}"}
-puts "Sum of conditional probabilities = #{posterior.reduce(0) {|acc, x| acc+=simplemodel.calculate_conditional_probability(posterior: x, prior: prior, ngram_model: 4)}}"
+puts "============ Revised conditional probabilities with Good-Turing discounted counts ================"
+prior = 'german'
+posterior = ['forces', 'failure', 'exactions', 'aggression', 'power', 'broadcast', 'troops', 'hands', 'and', 'war', 'air']
+posterior.each {|x| puts "P(#{x} | #{prior}) = #{simplemodel.calculate_conditional_probability(posterior: x, prior: prior, ngram_model: 2, discounted: true)}, discounted count = #{simplemodel.get_revised_counts(next_ngram: prior+' '+x)}"}
+puts "Sum of conditional probabilities = #{posterior.reduce(0) {|acc, x| acc+=simplemodel.calculate_conditional_probability(posterior: x, prior: prior, ngram_model: 2)}}"
+end
+
+#puts "Pkatz(of course was) = ..."
+phr='german forces are'
+x = simplemodel.calculate_katz_probability next_ngram: phr, ngram_model: 3
+puts "Pkatz(#{phr}) = #{x}"
